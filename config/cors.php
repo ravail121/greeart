72aa9efdc4555ef87827504817bbd07e702ad098
arjun-kohli95 <arjun.idigitalwebs@gmail.com>
2024-10-29 23:55:29 +0530

    cors.php file updated

--- config/cors.php
+++ config/cors.php
@@ -31,7 +31,8 @@ return [
     /*
      * Matches the request origin. `['*']` allows all origins. Wildcards can be used, eg `*.mydomain.com`
      */
-    'allowed_origins' => explode(',', env('FRONTEND_URL', '*', '5.196.64.87')),
+    // 'allowed_origins' => explode(',', env('FRONTEND_URL', '*', '5.196.64.87')),
+    'allowed_origins' => ['*'],
 
     /*
      * Patterns that can be used with `preg_match` to match the origin.
